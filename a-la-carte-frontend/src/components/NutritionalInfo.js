d212dd1337daf914b4ea408b4743cb0f51b9baf6
ericong18 <ericong18@gmail.com>
2019-02-16 11:42:24 -0800

    Created Nutritional Info page

--- a-la-carte-frontend/src/components/NutritionalInfo.js
+++ a-la-carte-frontend/src/components/NutritionalInfo.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import { connect } from 'react-redux'
 import './NutritionalInfo.css'
 
-export default class NutritionalInfo extends Component {
+const nutritionOne = require('../assets/images/nutrition-1.jpg')
+
+class NutritionalInfo extends Component {
+  handleSubmit = e => {
+    e.preventDefault()
+    
+    let calories = e.target.totalCalories.value
+    let meals = e.target.meals.value
+    let allergies = e.target.allergies.value
+  }
+
   render() {
     return (
       <div className="nutritional-info-container">
-        <Header/>
-        <div className="nutritional-info-title-container">
-          <h1 className="title">Nutritional Info</h1>
+        <div class="container-fluid">
+        	<div class="row">
+            <div class="col-md-8">
+              <Header/>
+              	<div class="row justify-content-center">
+                  <div className="nutritional-info-title-container">
+                  	  <h1 className="title">Nutritional Info</h1>
+                  	</div>
+                </div>
+                <div class="nutritional-info-form-container">
+                  <form className="nutritional-info-form" onSubmit={ this.handleSubmit }>
+                    <div class="row">
+                      <div className="col-md-12">
+                        <p className="info-caption">Enter your preferred total calories for the day.</p>
+                        <input className="form-control" id="total-calories" name="totalCalories" type="text" placeholder="Total calories..."/>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div className="col-md-12">
+                        <p className="info-caption">Enter your preferred number of meals for the day.</p>
+                        <input className="form-control" id="meals" name="meals" type="text" placeholder="Number of meals..."/>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div className="col-md-12">
+                        <p className="info-caption">Any allergies? (Separate items by comma)</p>
+                        <textarea class="form-control" placeholder="Enter your allergies here..." id="allergies" name="allergies" rows="3"></textarea>
+                      </div>
+                    </div>
+                    <div className="row justify-content-center">
+                      <button id="submit-button" type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                  </form>
+                </div>
+            </div>
+            <div className="col-md-4 image-filler"></div>
+          </div>
         </div>
       </div>
     )
   }
 }
+
+export default connect(null, null)(NutritionalInfo)
--- a-la-carte-frontend/src/components/PersonalInfo.js
+++ a-la-carte-frontend/src/components/PersonalInfo.js
@@ -20,61 +20,72 @@ class PersonalInfo extends Component {
     let feet = e.target.feet.value
     let inches = e.target.inches.value
 
-    console.log(gender, years, weight, feet, inches)
+    this.props.setGender(gender)
+    this.props.setAge(years)
+    this.props.setWeight(weight)
+    this.props.setFeet(feet)
+    this.props.setInches(inches)
+
+    window.location.href="/nutritional-info"
   }
 
   render() {
     return (
       <div className="personal-info-container">
         <div class="container-fluid">
-        	<Header/>
-        	<div class="row justify-content-center">
-            <div className="personal-info-title-container">
-            	  <h1 className="title">Personal Info</h1>
-            	</div>
-          </div>
-            <div class="personal-info-form-container">
-              <form className="personal-info-form" onSubmit={ this.handleSubmit }>
-                <div class="row">
-                  <div class="col-md-10">
-                    <p className="info-caption">Please select your gender.</p>
-                    <select className="form-control" name="gender" id="gender">
-                      <option>Male</option>
-                      <option>Female</option>
-                    </select>
+        	<div class="row">
+            <div class="col-md-8">
+              <Header/>
+              <div class="row justify-content-center">
+                <div className="personal-info-title-container">
+                    <h1 className="title">Personal Info</h1>
                   </div>
-                </div>
-                <div class="row">
-                  <div className="col-md-8">
-                    <p className="info-caption">Enter your age (in years).</p>
-                    <input className="form-control" id="years" name="years" type="text" placeholder="Years..."/>
+              </div>
+              <div class="personal-info-form-container">
+                <form className="personal-info-form" onSubmit={ this.handleSubmit }>
+                  <div class="row">
+                    <div class="col-md-8">
+                      <p className="info-caption">Please select your gender.</p>
+                      <select className="form-control" name="gender" id="gender">
+                        <option>Male</option>
+                        <option>Female</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
-                <div class="row">
-                  <div className="col-md-10">
-                    <p className="info-caption">Enter your height (ft, in).</p>
-                      <div class="row">
-                        <div class="col-md-6">
-                          <input className="form-control form-inline" id="feet" name="feet" type="text" placeholder="Feet..."/>
+                  <div class="row">
+                    <div className="col-md-8">
+                      <p className="info-caption">Enter your age (in years).</p>
+                      <input className="form-control" id="years" name="years" type="text" placeholder="Years..."/>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div className="col-md-10">
+                      <p className="info-caption">Enter your height (ft, in).</p>
+                        <div class="row">
+                          <div class="col-md-5">
+                            <input className="form-control form-inline" id="feet" name="feet" type="text" placeholder="Feet..."/>
+                          </div>
+                          <div class="col-md-5">
+                            <input className="form-control form-inline" id="inches" name="inches" type="text" placeholder="Inches..."/>
+                          </div>
                         </div>
-                        <div class="col-md-6">
-                          <input className="form-control form-inline" id="inches" name="inches" type="text" placeholder="Inches..."/>
-                        </div>
-                      </div>
+                    </div>
                   </div>
-                </div>
-                <div class="row">
-                  <div className="col-md-8">
-                    <p className="info-caption">Enter your weight (lbs).</p>
-                    <input className="form-control" id="weight" type="text" name="weight" placeholder="Weight..."/>
+                  <div class="row">
+                    <div className="col-md-8">
+                      <p className="info-caption">Enter your weight (lbs).</p>
+                      <input className="form-control" id="weight" type="text" name="weight" placeholder="Weight..."/>
+                    </div>
                   </div>
-                </div>
-                <div className="row justify-content-center">
-                  <button id="submit-button" type="submit" className="btn btn-primary">Next</button>
-                </div>
-              </form>
+                  <div className="row justify-content-center">
+                    <button id="submit-button" type="submit" className="btn btn-primary">Next</button>
+                  </div>
+                </form>
+              </div>
             </div>
+            <div className="col-md-4 image-filler"></div>
           </div>
+        </div>
       </div>
     )
   }
@@ -86,7 +97,7 @@ const mapDispatchToProps = dispatch => {
     setWeight: weight => { dispatch(setWeight(weight)) },
     setAge: age => { dispatch(setAge(age)) },
     setFeet: feet => { dispatch(setFeet(feet)) },
-    setAge: inches => { dispatch(setInches(inches)) }
+    setInches: inches => { dispatch(setInches(inches)) }
   }
 }
 
