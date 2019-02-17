5d42016b86baf90806d8247f3e09ddffd03d9bff
Justin Schopick <jscho004@ucr.edu>
2019-02-16 20:07:22 -0800

    Set up db connection

--- a-la-carte-api/connection.js
+++ a-la-carte-api/connection.js
@@ -1,11 +1,12 @@
 // Connect to the database
 const mysql = require('mysql');
+const db = require('./config/DB');
 
 const connection = mysql.createConnection({
-  host: '',
-  user: '',
-  password: '',
-  database: ''
+  host: db.host,
+  user: db.user,
+  password: db.password,
+  database: db.database
 });
 
 module.exports = connection;
