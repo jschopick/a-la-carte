7b24d63fd4c9fc47a67f01b96a949c469aa3e1c0
Justin Schopick <jscho004@ucr.edu>
2019-02-16 23:48:40 -0800

    Fixed post request to DB

--- a-la-carte-api/routes/user-info.js
+++ a-la-carte-api/routes/user-info.js
@@ -6,9 +6,9 @@ module.exports = function(router) {
     let info = req.body;
     console.log(info);
     let sql = "SET @calories = ?; SET @carbohydrates = ?; SET @proteins = ?; SET @fats = ?; SET @allergies = ?; SET @meals = ?; SET @activity = ?; SET @experience = ?; SET @weight = ?; SET @goalWeight = ?; SET @feet = ?; SET @inches = ?; SET @gender = ?; SET @age = ?; CALL UserAddOrUpdate(@calories, @carbohydrates, @proteins, @fats, @allergies, @meals, @activity, @experience, @weight, @goalWeight, @feet, @inches, @gender, @age);"
-    connection.query(sql, weight.weight, function(err, rows) {
+    connection.query(sql, [info.calories, info.carbohydrates, info.proteins, info.fats, info.allergies, info.meals, info.activity, info.experience, info.weight, info.goalWeight, info.feet, info.inches, info.gender, info.age], function(err, rows) {
       if(err) {
-        console.log('Error: could not execute query');
+        console.log(err);
         return res.status(500).send(err);
       } else {
         console.log('\nSuccessful Post to DB');
