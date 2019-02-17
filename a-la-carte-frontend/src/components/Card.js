8f687be41577378cc0dcac4ac622f1bc7b8adb23
ericong18 <ericong18@gmail.com>
2019-02-17 05:41:17 -0800

    Added lunch and dinner

--- a-la-carte-frontend/src/components/Card.js
+++ a-la-carte-frontend/src/components/Card.js
@@ -5,11 +5,11 @@ export default class Card extends Component {
   convertFoodString = foods => {
     let foodItems = ""
 
-    for (let i = 0; i < foods.length; ++i) {
-      foodItems += foods[i][0] + "(" +  foods[i][1] + ") "
+    for (let i = 0; i < foods.length - 1; ++i) {
+      foodItems += foods[i][0] + "(" +  foods[i][1] + "g) "
     }
     // return foodItems.split(" ").join(", ").substring(0, foodItems.length - 1)
-    return foodItems
+    return foodItems.split(" ").join(", ") + foods[foods.length - 1][0] + "(" +  foods[foods.length - 1][1] + ") "
   }
 
   render () {
@@ -17,9 +17,23 @@ export default class Card extends Component {
       <div className="card-container">
         <div class="container-fluid">
           <h1 className="restaurant-name">{ this.props.name }</h1>
-          <p className="meal-name">BREAKFAST</p>
-          <div class="text-container">
-            <p className="food-items">{ this.convertFoodString(this.props.breakfast) }</p>
+          <div class="meal-container">
+            <p className="meal-name">BREAKFAST</p>
+            <div class="text-container">
+              <p className="food-items">{ this.convertFoodString(this.props.breakfast) }</p>
+            </div>
+          </div>
+          <div class="meal-container">
+            <p className="meal-name">LUNCH</p>
+            <div class="text-container">
+              <p className="food-items">{ this.convertFoodString(this.props.breakfast) }</p>
+            </div>
+          </div>
+          <div class="meal-container">
+            <p className="meal-name">DINNER</p>
+            <div class="text-container">
+              <p className="food-items">{ this.convertFoodString(this.props.breakfast) }</p>
+            </div>
           </div>
         </div>
       </div>
